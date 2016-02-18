05bd6036d1c40f6f856b60474bf3e2f983f3c857
Victor De Blas <vdeblas@werfen.local>
2016-02-18 16:35:04 +0100

    Changed api calls

--- src/main-page/services/MainService.factory.js
+++ src/main-page/services/MainService.factory.js
@@ -21,11 +21,31 @@ function MainServiceFactory($http) {
 
 	function registerNewUser(userParams) {
 		var url = 'http://api.pascuajoven.com/api/' + 'newUser';
-		return $http.post(url, userParams);
+		var req = {
+			method: 'POST',
+			url: url,
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			data: messageParams
+		};
+		
+		return $http(req);
+		//return $http.post(url, userParams);
     }
 
     function sendMessage(messageParams) {
-		var url = 'http://api.pascuajoven.com/api/' + 'newContact';
-		return $http.post(url, messageParams);
+    	var url = 'http://api.pascuajoven.com/api/' + 'newContact';
+    	var req = {
+			method: 'POST',
+			url: url,
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			data: messageParams
+		};
+		
+		return $http(req);
+		//return $http.post(url, messageParams);
     }
 }
